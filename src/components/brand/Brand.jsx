31d7ac0e497e3963b42a14ff7a4def27315bbb1e
Dang Quang Khai <khaidq@vnpay.vn>
2023-02-22 16:13:34 +0700

    add image under header

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './brand.css'
+import React from "react";
+import "./brand.css";
+import * as importedImage from "./import";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="gtp3__brand section__padding">
+      <div>
+        <img src={importedImage.google} alt="google" />
+      </div>
+      <div>
+        <img src={importedImage.slack} alt="slack" />
+      </div>
+      <div>
+        <img src={importedImage.atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={importedImage.dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={importedImage.shopify} alt="shopify" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
